36a6cc03b9b174a87da793c59391e294b7c2b922
cxyang101 <cxyang9949@gmail.com>
2021-04-13 00:07:30 -0400

    Finished site

--- map.js
+++ map.js
@@ -36,6 +36,7 @@ var tooltip = d3.select("body").append("div")
             .attr("class", "tooltip")
             .style("opacity", 0);
 
+
 var plant_info = d3.select("#plant_info")
             .style("opacity", 0);
 
@@ -46,9 +47,9 @@ function zoomed() {
   }
 
 // THREAT CHART STUFF
-var tmargin = {top: 50, right: 40, bottom: 150, left: 40};
-var twidth = 400 - tmargin.left - tmargin.right;
-var theight = 500 - tmargin.top - tmargin.bottom;
+var tmargin = {top: 50, right: 30, bottom: 50, left: 140};
+var twidth = 550 - tmargin.left - tmargin.right;
+var theight = 350 - tmargin.top - tmargin.bottom;
 
 var svg2 = d3.select("#threat").append("svg")
     .attr("width", twidth + tmargin.left + tmargin.right)
@@ -56,11 +57,11 @@ var svg2 = d3.select("#threat").append("svg")
     .append("g")
     .attr("transform", "translate(" + tmargin.left + "," + tmargin.top + ")");
 
-var x = d3.scaleBand() // can use this instead of 1000 to have the max of data: d3.max(data, function(d) { return +d.price })
-    .range([0, twidth])
+var y = d3.scaleBand() // can use this instead of 1000 to have the max of data: d3.max(data, function(d) { return +d.price })
+    .range([theight, 0])
     .padding(0.1);
 
-var y = d3.scaleLinear().range([theight, 0]);
+var x = d3.scaleLinear().range([0, twidth]);
 
 var threatData, xAxis, yAxis;
 var threat_cts = {};
@@ -85,44 +86,45 @@ d3.csv("https://raw.githubusercontent.com/6859-sp21/a4-cindy/main/threats.csv", 
 
     threat_cts = d3.entries(threat_cts);
     threat_cts.sort(function(x, y){
-        return d3.descending(x.value, y.value);
+        return d3.ascending(x.value, y.value);
      })
-  
     // Scale the range of the data in the domains
-    x.domain(threat_cts.map(function(d) { return d.key; }));
-    y.domain([0, d3.max(threat_cts, function(d) { return d.value; })]);
+    y.domain(threat_cts.map(function(d) { return d.key; }));
+    x.domain([0, d3.max(threat_cts, function(d) { return d.value; })]);
   
     // append the rectangles for the bar chart
     svg2.selectAll("rect")
         .data(threat_cts)
         .enter()
         .append("rect")
-        .attr("x", function(d) { return x(d.key); })
-        .attr("width", x.bandwidth())
-        .attr("y", function(d) { return y(d.value); })
-        .attr("height", function(d) { return theight - y(d.value); })
+        .attr("y", function(d) { return y(d.key); })
+        .attr("height", y.bandwidth())
+        .attr("x", x(0))
+        .attr("width", function(d) { return x(d.value); })
         .attr("fill", "Crimson");
 
     svg2.append("text")
-        .attr("x", (twidth / 2))             
+        .attr("x", (twidth / 2) )             
         .attr("y", 0 - margin.top/2)
         .attr("text-anchor", "middle")  
         .style("font-size", "16px") 
+        .style("font-weight", "bold") 
         .attr("font-family", "Arial, Helvetica, sans-serif")  
         .text("Number of Species by Threat");
   
     // add the x Axis
     xAxis = svg2.append("g")
         .attr("class","xaxis")
-        .attr("transform", "translate(0," + theight + ")")
         .call(d3.axisBottom(x))
-        .selectAll("text")
-        .style("text-anchor", "end")
-        .attr("transform", "rotate(-65)");
+        .attr("transform", "translate(0," + theight + ")");
   
     // add the y Axis
     yAxis = svg2.append("g")
-        .call(d3.axisLeft(y));
+        .attr("class","yaxis")
+        .call(d3.axisLeft(y))
+        .selectAll("text")
+        .style("text-anchor", "end")
+        .attr("transform", "translate(0,0)");
 
 });
 
@@ -132,13 +134,14 @@ var svg3 = d3.select("#actions").append("svg")
     .append("g")
     .attr("transform", "translate(" + tmargin.left + "," + tmargin.top + ")");
 
-var x_a = d3.scaleBand() // can use this instead of 1000 to have the max of data: d3.max(data, function(d) { return +d.price })
-    .range([0, twidth])
+var y_a = d3.scaleBand() // can use this instead of 1000 to have the max of data: d3.max(data, function(d) { return +d.price })
+    .range([theight, 0])
     .padding(0.1);
 
-var y_a = d3.scaleLinear().range([theight, 0]);
+var x_a = d3.scaleLinear().range([0, twidth]);
 var actionData, xAxis_a, yAxis_a;
 var action_cts = {};
+
 
 d3.csv("https://raw.githubusercontent.com/6859-sp21/a4-cindy/main/actions.csv", function(error, data) {
     if (error) throw error;
@@ -157,22 +160,24 @@ d3.csv("https://raw.githubusercontent.com/6859-sp21/a4-cindy/main/actions.csv", 
 
     action_cts = d3.entries(action_cts);
     action_cts.sort(function(x, y){
-        return d3.descending(x.value, y.value);
+        return d3.ascending(x.value, y.value);
      })
   
+     
     // Scale the range of the data in the domains
-    x_a.domain(action_cts.map(function(d) { return d.key; }));
-    y_a.domain([0, d3.max(action_cts, function(d) { return d.value; })]);
+    y_a.domain(action_cts.map(function(d) { return d.key; }));
+    x_a.domain([0, d3.max(action_cts, function(d) { return d.value; })]);
   
+
     // append the rectangles for the bar chart
     svg3.selectAll("rect")
         .data(action_cts)
         .enter()
         .append("rect")
-        .attr("x", function(d) { return x_a(d.key); })
-        .attr("width", x_a.bandwidth())
-        .attr("y", function(d) { return y_a(d.value); })
-        .attr("height", function(d) { return theight - y_a(d.value); })
+        .attr("y", function(d) { return y_a(d.key); })
+        .attr("height", y_a.bandwidth())
+        .attr("x", x_a(0))
+        .attr("width", function(d) { return x_a(d.value); })
         .attr("fill", "#69b3a2");
 
     svg3.append("text")
@@ -180,21 +185,24 @@ d3.csv("https://raw.githubusercontent.com/6859-sp21/a4-cindy/main/actions.csv", 
         .attr("y", 0 - margin.top/2)
         .attr("text-anchor", "middle")  
         .style("font-size", "16px") 
+        .style("font-weight", "bold") 
         .attr("font-family", "Arial, Helvetica, sans-serif")    
         .text("Actions Taken");
   
     // add the x Axis
     xAxis_a = svg3.append("g")
         .attr("class","xaxis")
-        .attr("transform", "translate(0," + theight + ")")
         .call(d3.axisBottom(x_a))
-        .selectAll("text")
-        .style("text-anchor", "end")
-        .attr("transform", "rotate(-65)");
-  
+        .attr("transform", "translate(0," + theight + ")");
+        
     // add the y Axis
     yAxis_a = svg3.append("g")
-        .call(d3.axisLeft(y_a));
+        .attr("class","yaxis")
+        .attr("transform", "translate(0,0)")
+        .call(d3.axisLeft(y_a))
+        .selectAll("text")
+        .style("text-anchor", "end")
+        .attr("transform", "translate(0,0)");
 
 });
 
@@ -238,23 +246,24 @@ d3.csv("https://raw.githubusercontent.com/6859-sp21/a4-cindy/main/plants.csv", f
 
         // Add the area
         svg4.append("path")
-        .datum(plant_cts)
-        .attr("class","areaChart")
-        .attr("fill", "#69b3a2")
-        .attr("stroke", "#69b3a2")
-        .attr("d", d3.area()
-            .x(function(d) { return x_p(d.key) })
-            .y1(function(d) { return y_p(d.value) })
-            .y0(theight)
-            );
+            .datum(plant_cts)
+            .attr("class","lineTest")
+            .attr("fill", "none")
+            .attr("stroke", "steelblue")
+            .attr("stroke-width", 1.5)
+            .attr("d", d3.line()
+                .x(function(d) { return x_p(d.key) })
+                .y(function(d) { return y_p(d.value) })
+            );  
 
         svg4.append("text")
             .attr("x", (twidth / 2))             
             .attr("y", 0 - margin.top/2)
             .attr("text-anchor", "middle")  
             .style("font-size", "16px")   
+            .style("font-weight", "bold") 
             .attr("font-family", "Arial, Helvetica, sans-serif")  
-            .text("Number of Species Last Seen");
+            .text("Date of Extinction");
     
          // Add X axis --> it is a date format
          xAxis_p = svg4.append("g")
@@ -263,25 +272,27 @@ d3.csv("https://raw.githubusercontent.com/6859-sp21/a4-cindy/main/plants.csv", f
                     .call(d3.axisBottom(x_p))
                     .selectAll("text")
                     .style("text-anchor", "end")
-                    .attr("transform", "rotate(-25)");
+                    .attr("transform", "rotate(-15)");
 
-         yAxis_p = svg4.append("g").call(d3.axisLeft(y_p));
+         yAxis_p = svg4.append("g").attr("class", "yaxis").call(d3.axisLeft(y_p));
+         
+         svg4.append("text")
+            .attr("class", "yaxis")
+            .attr("text-anchor", "middle")
+            .attr("y", -45)
+            .attr("x", -130)
+            .attr("dy", ".75em")
+            .attr("font-family", "Arial, Helvetica, sans-serif")  
+            .attr("transform", "rotate(-90)")
+            .text("Number of species");
 
 })
 
 var lwidth = 400,
     lheight = 800;
 
-var species = d3.select("#species")
+var species = d3.select("#species").style("opacity", 0);
 var species_dict = {};
-
-// species.select("text")
-//     .attr("x", (lwidth / 2))             
-//     .attr("y", 0 - margin.top/2)
-//     .attr("text-anchor", "middle")  
-//     .style("font-size", "16px")   
-//     .attr("font-family", "Arial, Helvetica, sans-serif")  
-//     .text("");
 
 clicked = function(d) {
 
@@ -302,21 +313,21 @@ clicked = function(d) {
 
     new_threat_cts = d3.entries(new_threat_cts);
     new_threat_cts.sort(function(x, y){
-        return d3.descending(x.value, y.value);
+        return d3.ascending(x.value, y.value);
      })
+    
+    x.domain([0, d3.max(new_threat_cts, function(datum) { return datum.value; })]);
+    y.domain(new_threat_cts.map(function(datum) { return datum.key; }));
 
-    x.domain(new_threat_cts.map(function(datum) { return datum.key; }));
-
-    svg2.selectAll(".xaxis")
-        .transition()
+    xAxis.transition()
         .duration(1000)
-        .call(d3.axisBottom(x))
-        .selectAll("text")
-        .style("text-anchor", "end")
-        .attr("transform", "rotate(-65)");
-    y.domain([0, d3.max(new_threat_cts, function(datum) { return datum.value; })]);
-    yAxis.transition().duration(100).call(d3.axisLeft(y));
+        .call(d3.axisBottom(x));
 
+    svg2.selectAll(".yaxis").transition()
+        .duration(100)
+        .call(d3.axisLeft(y))
+        .selectAll("text")
+        .style("text-anchor", "end");
 
     var u = svg2.selectAll("rect").data(new_threat_cts)
     
@@ -325,14 +336,13 @@ clicked = function(d) {
         .merge(u)
         .transition()
         .duration(1000)
-        .attr("x", function(datum) { return x(datum.key); })
-        .attr("width", x.bandwidth())
-        .attr("y", function(datum) { return y(datum.value); })
-        .attr("height", function(datum) { return theight - y(datum.value); })
+        .attr("y", function(datum) { return y(datum.key); })
+        .attr("height", y.bandwidth())
+        .attr("x", function(datum) { return x(0) })
+        .attr("width", function(datum) { return x(datum.value); })
         .attr("fill", "Crimson");
     
     u.exit().remove()
-
     threat_cts = new_threat_cts;
 
     // 2. Update Actions Taken
@@ -351,20 +361,20 @@ clicked = function(d) {
 
     new_action_cts = d3.entries(new_action_cts);
     new_action_cts.sort(function(x, y){
-        return d3.descending(x.value, y.value);
+        return d3.ascending(x.value, y.value);
      })
 
-    x_a.domain(new_action_cts.map(function(datum) { return datum.key; }));
+    y_a.domain(new_action_cts.map(function(datum) { return datum.key; }));
 
-    svg3.selectAll(".xaxis")
+    svg3.selectAll(".yaxis")
         .transition()
         .duration(1000)
-        .call(d3.axisBottom(x_a))
+        .call(d3.axisLeft(y_a))
         .selectAll("text")
-        .style("text-anchor", "end")
-        .attr("transform", "rotate(-65)");
-    y_a.domain([0, d3.max(new_action_cts, function(datum) { return datum.value; })]);
-    yAxis_a.transition().duration(100).call(d3.axisLeft(y_a));
+        .style("text-anchor", "end");
+
+    x_a.domain([0, d3.max(new_action_cts, function(datum) { return datum.value; })]);
+    xAxis_a.transition().duration(100).call(d3.axisBottom(x_a));
 
     var u3 = svg3.selectAll("rect").data(new_action_cts)
     
@@ -373,10 +383,10 @@ clicked = function(d) {
         .merge(u3)
         .transition()
         .duration(1000)
-        .attr("x", function(datum) { return x_a(datum.key); })
-        .attr("width", x_a.bandwidth())
-        .attr("y", function(datum) { return y_a(datum.value); })
-        .attr("height", function(datum) { return theight - y_a(datum.value); })
+        .attr("y", function(datum) { return y_a(datum.key); })
+        .attr("height", y_a.bandwidth())
+        .attr("x", x(0))
+        .attr("width", function(datum) { return x_a(datum.value); })
         .attr("fill", "#69b3a2");
     
     u3.exit().remove()
@@ -395,6 +405,8 @@ clicked = function(d) {
             new_plant_cts[year] = new_plant_cts[year] + 1;
         }
     });
+    if (new_plant_cts.hasOwnProperty("NA")) {delete new_plant_cts["NA"]; }
+    console.log(Object.keys(new_plant_cts))
 
     new_plant_cts = d3.entries(new_plant_cts);
     new_plant_cts.sort(function(first, second) {
@@ -404,24 +416,26 @@ clicked = function(d) {
     y_p.domain([0, d3.max(new_plant_cts, function(datum) { return datum.value; })]);
     yAxis_p.transition().duration(100).call(d3.axisLeft(y_p));
 
-    // Add the area
-    svg4.selectAll(".areaChart").remove();
-    
+    svg4.selectAll(".lineTest").remove();
+
     svg4.append("path")
-    .datum(new_plant_cts)
-    .attr("class", "areaChart")
-    .attr("fill", "#69b3a2")
-    .attr("stroke", "#69b3a2")
-    .attr("d", d3.area()
-        .x(function(d) { return x_p(d.key) })
-        .y1(function(d) { return y_p(d.value) })
-        .y0(theight)
-        );
+        .datum(new_plant_cts)
+        .attr("class","lineTest")
+        .attr("fill", "none")
+        .attr("stroke", "steelblue")
+        .attr("stroke-width", 1.5)
+        .attr("d", d3.line()
+            .x(function(d) { return x_p(d.key) })
+            .y(function(d) { return y_p(d.value) })
+        ); 
+
+
     plant_cts = new_plant_cts;
     
     species_dict = d3.map(newSpecies, function(d){return d.binomial_name;});
     
-    species.select("p").text(`Extinct Species in ${d.properties.name}`);
+    species.transition().duration(10).style("opacity", .9);;
+    species.select("p").text(`Extinct Species in ${d.properties.name}`).style("font-weight", "bold");
     species.select("ol").selectAll("li").remove();
     species.select("ol")
             .selectAll("li")
@@ -461,10 +475,149 @@ clicked = function(d) {
         .duration(750)
         // .call( zoom.transform, d3.zoomIdentity.translate(0, 0).scale(1) ); // not in d3 v4
         .call( zoom.transform, d3.zoomIdentity ); // updated for d3 v4
-  }
+
+    species.transition()
+            .duration(10)
+            .style("opacity", 0);
+
+    // 1 - THREAT GRAPH
+    const newData = threatData
+    var new_threat_cts = {};
+
+    newData.forEach( function(dd) {
+        var ttype = dd.threat_type;
+        var threatened = +dd.threatened;
+        if (new_threat_cts[ttype] === undefined){
+            new_threat_cts[ttype] = 0;
+        } else if (threatened==1) {
+            new_threat_cts[ttype] = new_threat_cts[ttype] + 1;
+        }
+    });
+
+    new_threat_cts = d3.entries(new_threat_cts);
+    new_threat_cts.sort(function(x, y){
+        return d3.ascending(x.value, y.value);
+     })
+    
+    x.domain([0, d3.max(new_threat_cts, function(datum) { return datum.value; })]);
+    y.domain(new_threat_cts.map(function(datum) { return datum.key; }));
+
+    xAxis.transition()
+        .duration(1000)
+        .call(d3.axisBottom(x));
+
+    svg2.selectAll(".yaxis").transition()
+        .duration(100)
+        .call(d3.axisLeft(y))
+        .selectAll("text")
+        .style("text-anchor", "end");
+
+    var u = svg2.selectAll("rect").data(new_threat_cts)
+    
+    u.enter()
+        .append("rect")
+        .merge(u)
+        .transition()
+        .duration(1000)
+        .attr("y", function(datum) { return y(datum.key); })
+        .attr("height", y.bandwidth())
+        .attr("x", function(datum) { return x(0) })
+        .attr("width", function(datum) { return x(datum.value); })
+        .attr("fill", "Crimson");
+    
+    u.exit().remove()
+    threat_cts = new_threat_cts;
+
+    // 2. Update Actions Taken
+    const newActions = actionData;
+    var new_action_cts = {};
+
+    newActions.forEach( function(d) {
+        var atype = d.action_type;
+        var action_taken = +d.action_taken;
+        if (new_action_cts[atype] === undefined){
+            new_action_cts[atype] = 0;
+        } else if (action_taken==1) {
+            new_action_cts[atype] = new_action_cts[atype] + 1;
+        }
+    });
+
+    new_action_cts = d3.entries(new_action_cts);
+    new_action_cts.sort(function(x, y){
+        return d3.ascending(x.value, y.value);
+     })
+
+    y_a.domain(new_action_cts.map(function(datum) { return datum.key; }));
+
+    svg3.selectAll(".yaxis")
+        .transition()
+        .duration(1000)
+        .call(d3.axisLeft(y_a))
+        .selectAll("text")
+        .style("text-anchor", "end");
+
+    x_a.domain([0, d3.max(new_action_cts, function(datum) { return datum.value; })]);
+    xAxis_a.transition().duration(100).call(d3.axisBottom(x_a));
+
+    var u3 = svg3.selectAll("rect").data(new_action_cts)
+    
+    u3.enter()
+        .append("rect")
+        .merge(u3)
+        .transition()
+        .duration(1000)
+        .attr("y", function(datum) { return y_a(datum.key); })
+        .attr("height", y_a.bandwidth())
+        .attr("x", x(0))
+        .attr("width", function(datum) { return x_a(datum.value); })
+        .attr("fill", "#69b3a2");
+    
+    u3.exit().remove()
+
+    action_cts = new_action_cts;
+
+    // 3. Update Species Last Seen
+    const newSpecies = actionData;
+    var new_plant_cts = {};
+
+    newSpecies.forEach( function(d) {
+        var year = d.year_last_seen;
+        if (new_plant_cts[year] === undefined){
+            new_plant_cts[year] = 0;
+        } else {
+            new_plant_cts[year] = new_plant_cts[year] + 1;
+        }
+    });
+    if (new_plant_cts.hasOwnProperty("NA")) {delete new_plant_cts["NA"]; }
+    console.log(Object.keys(new_plant_cts))
+
+    new_plant_cts = d3.entries(new_plant_cts);
+    new_plant_cts.sort(function(first, second) {
+        return date_ranges.indexOf(first.key) - date_ranges.indexOf(second.key);
+      });
+    // Add Y axis
+    y_p.domain([0, d3.max(new_plant_cts, function(datum) { return datum.value; })]);
+    yAxis_p.transition().duration(100).call(d3.axisLeft(y_p));
+
+    svg4.selectAll(".lineTest").remove();
+
+    svg4.append("path")
+        .datum(new_plant_cts)
+        .attr("class","lineTest")
+        .attr("fill", "none")
+        .attr("stroke", "steelblue")
+        .attr("stroke-width", 1.5)
+        .attr("d", d3.line()
+            .x(function(d) { return x_p(d.key) })
+            .y(function(d) { return y_p(d.value) })
+        ); 
+
+
+    plant_cts = new_plant_cts;
+
+    }
 
   function display_plant(d) {
-    console.log(species_dict.get(d))
     var group = species_dict.get(d)["group"];
     var red_list_category = species_dict.get(d)['red_list_category'];
     var threats = [];
@@ -487,7 +640,7 @@ clicked = function(d) {
   }
 
 d3.queue()
-  .defer(d3.json, "https://raw.githubusercontent.com/6859-sp21/a4-cindy/main/continents.geojson", )
+  .defer(d3.json, "https://raw.githubusercontent.com/6859-sp21/a4-cindy/main/world.geojson", )
   .defer(d3.csv, "https://raw.githubusercontent.com/6859-sp21/a4-cindy/main/plants_by_country.csv", function(d) { data.set(d.code, +d.count); })
   .await(ready);
 
